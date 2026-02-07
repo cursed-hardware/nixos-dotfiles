3d3c2ac4dc6b36ab5d91e1ead1afc829d68b9409
Danil <pass>
2026-02-07 22:35:51 +0300

    Add nixos config and ugly readme

---
+++ README.md
@@ -0,0 +1,5 @@
+sudo ln -s /home/core/.dotfiles/configuration.nix /etc/nixos/.
+
+and
+
+stow -R -t -v ~/.config .
---
+++ configuration.nix
@@ -0,0 +1,95 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Moscow";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.core = {
+    isNormalUser = true;
+    description = "core";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+    shell = pkgs.fish;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+  services.gnome.core-apps.enable = false;
+  services.gnome.core-developer-tools.enable = false;
+  services.gnome.games.enable = false;
+  environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs ];  
+
+  programs.fish.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    # Gnome extension
+    gnomeExtensions.blur-my-shell
+
+    # GUI
+    telegram-desktop
+    firefox
+    obsidian
+
+    # Terminal env
+    alacritty
+    ranger
+    helix
+    tmux
+
+    # Dev
+    git
+    gh
+    curl
+    gcc
+    clang
+    clang-tools
+    gnumake
+    
+    # CLI
+    fastfetch
+    stow
+    yandex-disk
+
+    # FOnt
+    maple-mono.NF
+  ];
+
+  system.stateVersion = "25.11"; # Did you read the comment?
+}
